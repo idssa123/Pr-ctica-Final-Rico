5601e1e93a830b4de2c83b32b27bc8b26b43397e
idssa123 <idssa123@github.com>
2025-12-06 10:51:52 +0100

    Agregar clase principal de la aplicación Spring Boot

---
+++ src/main/java/com/rcdcoruna/RcdApiApplication.java
@@ -0,0 +1,13 @@
+package com.rcdcoruna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcdApiApplication.class, args);
+    }
+
+}
